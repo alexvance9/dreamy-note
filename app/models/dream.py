36b25375497604022057c28627bc316bbc8c2201
alexvance9 <alexvance9@gmail.com>
2023-01-24 15:25:02 -0800

    made dreams table and added rel to users

---
+++ app/models/dream.py
@@ -0,0 +1,21 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Dream(db.Model):
+    __tablename__ = 'dreams'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    # dreamer_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    dreamer = db.relationship("User", back_populates="dreams")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'date': self.date,
+            'body': self.body
+        }
