831ea3601aaa57aa2ed6c47034cceb956260846c
alexvance9 <alexvance9@gmail.com>
2023-01-26 11:11:56 -0800

    details and edit switch

--- react-app/src/components/DreamDetail/index.js
+++ react-app/src/components/DreamDetail/index.js
@@ -1,6 +1,39 @@
+import { useState } from "react"
+
 const DreamDetail = ({dream}) => {
+
+    const [isEdit, setIsEdit] = useState(false)
+
+
+    const openEditor = async (e) => {
+        e.preventDefault()
+        setIsEdit(true)
+    }
+
+    const saveChanges = async(e) => {
+        e.preventDefault()
+        setIsEdit(false)
+    }
+
+    let detailComponents;
+    if (isEdit) {
+        detailComponents = (
+            <>
+            <div>this will be the edit a dream form</div>
+            <button onClick={saveChanges}>this will save the changes</button>
+            </>
+        )
+    } else {
+        detailComponents = (
+            <>
+                <div>this will be the detail page</div>
+                <button onClick={openEditor}>edit the dream</button>
+            </>
+        )
+    }
+
     return (
-        <div>Heres the details of this dream</div>
+        <div>{detailComponents}</div>
     )
 }
 
