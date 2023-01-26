5978a3f760814e0a7ada6cd170ed31e2d30c81bc
alexvance9 <alexvance9@gmail.com>
2023-01-25 17:39:32 -0800

    connected dream detail to dream tab

--- react-app/src/components/DreamsTab/index.js
+++ react-app/src/components/DreamsTab/index.js
@@ -5,16 +5,27 @@ import DreamNav from "./DreamsNav"
 import './DreamsTab.css'
 
 const DreamsTab = () => {
-    let params = useParams()
-    console.log(params)
+    const sessionUser = useSelector(state => state.session.user);
+    const dreams = sessionUser.dreams
 
-   const sessionUser = useSelector(state => state.session.user);
-   const dreams = sessionUser.dreams
+    let params = useParams()
+    let currentDreamId;
+    if (Object.keys(params).length){
+        currentDreamId = Number(params.dreamId)
+        // console.log(currentDream)
+    } else {
+        // console.log("false!")
+        currentDreamId = dreams[0].id
+    }
+
+    // console.log("dream id", currentDreamId)
+    let currentDream = dreams.filter(dream => dream.id === currentDreamId)
+    // console.log("dream", currentDream)
 
     return (
-        <div className="dreams-tab-container"> 
+        <div className="dreams-tab-container flex"> 
             <DreamNav dreams={dreams}/>
-            <DreamDetail />
+            <DreamDetail dream={currentDream}/>
         </div>
     )
 }
