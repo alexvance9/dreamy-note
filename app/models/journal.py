b77e242e4493f4828bacbdb02a6b9e6b2213400c
alexvance9 <alexvance9@gmail.com>
2023-01-29 13:57:15 -0800

    switching to journal branch

---
+++ app/models/journal.py
@@ -0,0 +1,11 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Journal(db.Model):
+    __tablename__ = 'journals'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    date = db.Column(db.Date, nullable=False)
