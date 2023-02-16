c410f4342c61ad7bf6732d7c385598137cffea67
alexvance9 <alexvance9@gmail.com>
2023-02-16 13:34:25 -0800

    dashboard styling

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,15 +1,38 @@
-import sheep from '../../assets/sheep.png'
+// import sheep from '../../assets/sheep.png'
+import parse from 'html-react-parser';
+import { useSelector } from 'react-redux'
+import moment from "moment"
+import './Dashboard.css'
 
 const Dashboard = () => {
+
+    // random dream
+    const dreams = useSelector(state => state.session.user.dreams)
+    // console.log(dreams)
+    const randomDream = dreams[Math.floor(Math.random()*dreams.length)];
+    console.log(randomDream)
+    
    
         return (
-        <div className="journal-detail-container-extra">
-            <div className='sheep-no-dreams'>
-                <img className='user-sheep-img' alt='sheep icon' src={sheep} />
+        <div className="dashboard-container ">
+            <div className="dashboard-header">
+                <h2>Welcome to your Dreams</h2>
+                <div className='divider'></div>
+                <div>Today is {moment().format('dddd MMMM Do, YYYY')}</div>
             </div>
-            <div className='not-found-container flexcol'>
-                <h1>Dashboard Under Construction</h1>
-                <p>Head to the Dreams tab to begin.</p>
+            <div className="widget-container">
+                <div className="dream-widget">
+                    <h3>Remember this Dream?</h3>
+                        <div className='divider'></div>
+                    <h4>{randomDream.title}</h4>
+                    <div>{moment(randomDream.date, "YYYY-MM-DD").format("MM/DD/YYYY")}</div>
+                    <div className='dream-widget-body'>{parse(randomDream.body)}</div>
+                </div>
+                <div className="tags-widget">
+                    <h3>Coming Soon</h3>
+                     <div className='divider'></div>
+                    <div>Track recurring themes in your Dreams with tags!</div>
+                </div>
             </div>
         </div>
     
