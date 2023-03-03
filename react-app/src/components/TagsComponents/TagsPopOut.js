2ae17b466356ddc3f09e0363684aa497dff85208
alexvance9 <alexvance9@gmail.com>
2023-03-02 18:43:48 -0800

    create tag thunk and tags list displayed

--- react-app/src/components/TagsComponents/TagsPopOut.js
+++ react-app/src/components/TagsComponents/TagsPopOut.js
@@ -1,17 +1,27 @@
-
+import OpenModalButton from '../OpenModalButton'
+import CreateTagModal from './CreateTagModal';
 
 const TagsPopOut = ({setShowMenu, tags}) => {
     // console.log(tags)
-    const handleAddTag = (e) => {
-        e.preventDefault()
-
-    }
+    const tagsArr = Object.values(tags.tags)
+    // console.log(tagsArr)
+    
 
     return(
         <div className="tags-pop-out">
             <button onClick={setShowMenu}><i className="fa-solid fa-xmark fa-xl"></i></button>
             <h2>Tags</h2>
-            
+            <OpenModalButton 
+                buttonText="Add a Tag"
+                modalComponent={<CreateTagModal/>} 
+                />
+            <div className='tags-list-container'>
+            {tagsArr.map(tag => (
+                <div key={tag.id}>
+                    {tag.name}
+                    </div>
+            ))}
+            </div>
         </div>
     )
 }
--- react-app/src/store/tags.js
+++ react-app/src/store/tags.js
@@ -1,5 +1,6 @@
 // action variables
 const LOAD_TAGS = 'tags/LOAD_TAGS'
+const CREATE_TAG = 'tags/CREATE_TAG'
 
 // actions
 const loadTags = (tags) => ({
@@ -7,15 +8,20 @@ const loadTags = (tags) => ({
     payload: tags
 })
 
+const createTag = (tag) => ({
+    type: CREATE_TAG,
+    payload: tag
+})
 // thunks
 export const thunkLoadTags = () => async (dispatch) => {
     const response = await fetch('/api/tags')
     if (response.ok){
-        const tags = response.json()
+        const tags = await response.json()
+        console.log('tags at thunk: ', tags)
         dispatch(loadTags(tags))
         return null
     } else if (response.status < 500) {
-        const data = response.json()
+        const data = await response.json()
         if (data.errors) {
             return data.errors
         }
@@ -24,12 +30,42 @@ export const thunkLoadTags = () => async (dispatch) => {
     }
 }
 
-const initialState = { tags: []}
+export const thunkCreateTag = (name) => async (dispatch) => {
+    const response = await fetch('/api/tags', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name
+        })
+    })
+    if (response.ok){
+        const data = await response.json()
+        dispatch(createTag(data))
+        return null
+    } else if (response.status < 500) {
+        const data = await response.json()
+        if (data.errors) {
+            return data
+        }
+    } else {
+        return ['an error ocurred at add journals, please try again.']
+    }
+}
+
+const initialState = { tags: {}}
 export default function reducer(state = initialState, action){
     switch (action.type) {
         case LOAD_TAGS: {
-            const newState = { tags: [] }
-            newState.tags = action.payload.tags
+            const newState = { tags: {} }
+            console.log('action.payload:', action.payload)
+            action.payload.forEach(tag => newState.tags[tag.id] = tag)
+            return newState;
+        } 
+        case CREATE_TAG: {
+            const newState = { tags: {...state.tags} }
+            newState.tags[action.payload.id] = action.payload;
             return newState;
         }
         default:
