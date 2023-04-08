154c07dc0580a321a9ed6d6afe2f1b4c24985a75
alexvance9 <alexvance9@gmail.com>
2023-04-08 15:18:45 -0700

    refactor filter component to include journals

--- react-app/src/components/DreamsTab/FilterComponent.js
+++ react-app/src/components/DreamsTab/FilterComponent.js
@@ -5,22 +5,42 @@ import DreamNav from "./DreamsNav";
 const FilterComponent = ({dreams}) => {
     const {currentFilters, setCurrentFilters} = useFilter()
     
-    let filteredDreams;
-    if (currentFilters.length){
-        filteredDreams = dreams.filter(dream => {
-            const filter = currentFilters[0]
-        return dream.tags.some(tag => tag.id === filter.id)   
-        })
+    // let filteredDreams;
+    // if (currentFilters.tags.length || currentFilters.journal.length){
+        // filteredDreams = dreams.filter(dream => {
+        //     if (this.journal.length && this.tags.length) {
+                
+        //     }
+        let filteredDreams = dreams.filter(function (dream) {
+            // console.log(this)
+            if (this.journal.length && this.tags.length) {
+                return dream.journalId === this.journal[0] && dream.tags.some(tag => tag === this.tags[0])
+            } else if (this.journal.length && !this.tags.length) {
+                return dream.journalId === this.journal[0]
+            } else if (!this.journal.length && this.tags.length) {
+                return dream.tags.some(tag => tag === this.tags[0])
+            } else {
+                return dream;
+            }
+
+        }, currentFilters)
+        //     const filter = currentFilters.tags[0]
+        // return dream.tags.some(tag => tag.id === filter.id)   
+
         // console.log(filteredDreams)
-    } else {
-        filteredDreams = dreams
-    }
+    // } else {
+    //     filteredDreams = dreams
+    // }
     
 
 
-    const handleClear = (e) => {
+    const handleTagClear = (e) => {
         e.preventDefault()
-       return setCurrentFilters([])
+       return setCurrentFilters(filters =>{ return {...filters, tags: []}})
+    }
+    const handleJournalClear = (e) => {
+        e.preventDefault()
+       return setCurrentFilters(filters =>{ return {...filters, journal: []}})
     }
 
     return (
@@ -31,12 +51,17 @@ const FilterComponent = ({dreams}) => {
                 <h2>My Dreams</h2>
                 </div>
             </div>
-            {currentFilters.length > 0 && 
+            {(currentFilters.journal.length || currentFilters.tags.length) && 
                 <div className="filter-container">
                     <h3>Filters</h3>
-                {currentFilters.map(tag => (
+                {currentFilters.tags.map(tag => (
                     <div className="filter" key={tag.id}>{tag.name}
-                        <button onClick={handleClear}><i className="fa-solid fa-xmark"></i></button>
+                        <button onClick={handleTagClear}><i className="fa-solid fa-xmark"></i></button>
+                    </div>
+                ))}
+                {currentFilters.journal.map(journal => (
+                    <div className="filter" key={journal.id}>{journal.name}
+                        <button onClick={handleJournalClear}><i className="fa-solid fa-xmark"></i></button>
                     </div>
                 ))}
                 </div>}
